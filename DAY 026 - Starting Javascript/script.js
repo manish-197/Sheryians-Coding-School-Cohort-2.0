868c0469313263c26a9b76ff1b9a6b14a3aeb4c3
Manish Patil <manishpatil0738@gmail.com>
2025-10-29 13:05:25 +0530

    all comit

---
+++ DAY 026 - Starting Javascript/script.js
@@ -0,0 +1,5 @@
+console.log("connected");
+console.log("Js file loaded successfully");
+console.log("Javascript is running");
+let a = prompt("enter you name ");
+alert("your name is " + a);
---
+++ DAY 027 - Javascript Basics/script.js
@@ -0,0 +1,25 @@
+console.log("js is connected");
+
+// variables 
+
+var myName = "John Doe";
+let age = 30;
+const pi = 3.14;    
+
+console.log("Name:", myName);
+console.log("Age:", age);
+console.log("PI:", pi);
+
+// console, alert, promt 
+
+// console.log("This is a console log message.");
+// alert("Welcome");
+// let a = prompt("Enter Your Name:"); // promt alway input as string 
+// alert("hello " + a);
+
+// strings 
+
+let firstName = "Manish";
+let lastName = "Patil";
+
+console.log("Full Name:", firstName + " " + lastName);
