33ecaff872a5181ba548d50ccedf1688c4f04c27
Manish Patil <manishpatil0738@gmail.com>
2025-10-29 13:12:05 +0530

    DAY 028 - COMPLETE

---
+++ DAY 028 - Datatypes in JS/script.js
@@ -0,0 +1,25 @@
+console.log("Datatypes in JS");
+
+// Intege (numbers)
+
+let number = 197;
+
+// float 
+
+let float = 19.7;
+
+// String
+
+let str = "Manish";
+
+// Boolean
+
+let boolean = true;
+
+//Null 
+
+let empty = null;
+
+// Undefined
+
+let undef;
