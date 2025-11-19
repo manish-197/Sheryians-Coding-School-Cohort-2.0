cdf83f575592022006a82956e3f92227ca915540
Manish Patil <manishpatil0738@gmail.com>
2025-11-19 15:25:10 +0530

    DAY 029 - COMPLETE

---
+++ JAVASCRIPT/DAY 029 - Practise of Datatypes/script.js
@@ -0,0 +1,15 @@
+let age = 25;
+
+let name = "Manish";
+
+let isStudent = true;
+
+let skills = ["js", "html"];
+
+console.log(typeof age);
+
+age = "Twenty Five";
+
+console.log(typeof age);
+
+console.log(25 + "Manish");
