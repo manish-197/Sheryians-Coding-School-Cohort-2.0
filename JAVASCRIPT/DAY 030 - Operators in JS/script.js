7edb2e6d6c4b9170af769562a4e02199aee1f688
Manish Patil <manishpatil0738@gmail.com>
2025-11-19 15:39:34 +0530

    DAY 030 - COMPLETE

---
+++ JAVASCRIPT/DAY 030 - Operators in JS/script.js
@@ -0,0 +1,65 @@
+let a = 20;
+
+let b = 10;
+
+// Arithmetic Operators 
+
+console.log("The value of a + b is ", a + b);
+console.log("The value of a - b is ", a - b);
+console.log("The value of a * b is ", a * b);
+console.log("The value of a / b is ", a / b);
+console.log("The value of a % b is ", a % b);
+console.log("The value of a ** b is ", a ** b);
+console.log("The value of a ++ is ", a++);
+console.log("The value of a -- is ", a--);
+console.log("The value of ++a is ", ++a);
+console.log("The value of --a is ", --a);
+
+// Assignment Operators
+
+let c = b;
+console.log(c);
+c += b;
+console.log(c);
+c -= b;
+console.log(c);
+c *= b;
+console.log(c);
+c /= b;
+console.log(c);
+c %= b;
+console.log(c);
+c **= b;
+console.log(c);
+
+// Comparison Operators
+
+console.log(a === b);
+console.log(a == b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a > b);
+console.log(a < b);
+console.log(a != b);
+
+// Logical Operators
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+console.log(!false);
+console.log(!true);
+
+// Bitwise Operators
+
+console.log(a & b);
+console.log(a | b);
+console.log(a ^ b);
+console.log(~a);
+console.log(a << 2);
+console.log(a >> 2);
+console.log(a >>> 2);
+
